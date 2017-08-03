26a1c0cc99aed3062595f64fd929bee2b0a01652
Yu Lin <yu@Yus-MacBook-Pro.local>
2017-08-02 22:01:48 -0400

    refactor: removed unused styles

--- App.js
+++ App.js
@@ -7,22 +7,10 @@ export default class App extends React.Component {
     return (
       <View>
         <Navbar></Navbar>
-        <Text style={styles.bigRed}>Changesss you make will automatically reload.</Text>
+        <Text>Changesss you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bigRed: {
-    backgroundColor: 'red'
-  }
-});
 
